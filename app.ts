2e1ec7c1a66dd8894b65e5a94ac35bbedcc3f738
mlevitorresv <levitorres66@gmail.com>
2023-12-15 16:33:55 +0100

    ADD: route /levi

--- app.ts
+++ app.ts
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/levi', (req, res) => {
+  res.send('Hello Leví!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
